072a3acd60b5631e63737f47b46ff2b440be3cd5
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-12 15:24:59 +0200

    Change space unit names.

--- resources/views/header/default.php
+++ resources/views/header/default.php
@@ -22,12 +22,12 @@
 <body <?php hybrid_attr( 'body' ); ?>>
 
 <div class="app">
-	<header class="app-header">
+	<header class="app-header flex justify-between items-center mx-auto max-width-1 px-3 py-4">
 		<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'uuups' ); ?></a>
 
 		<div class="app-header__branding">
-			<h1 class="app-header__title"><a href="<?= esc_url( home_url() ) ?>"><?php bloginfo( 'name' ); ?></a></h1>
-			<p class="app-header__description"><?php bloginfo( 'description' ); ?></p>
+			<h1 class="app-header__title mb-0 h3"><a href="<?= esc_url( home_url() ) ?>"><?php bloginfo( 'name' ); ?></a></h1>
+			<p class="app-header__description mb-0"><?php bloginfo( 'description' ); ?></p>
 		</div>
 
 		<?php ABC\render_view( 'menu', 'primary', [ 'name' => 'primary' ] ); ?>
