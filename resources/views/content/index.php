f348c4b7ce827ac61281a2c9b0e7bb5d04a8ff4d
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-22 09:23:51 +0200

    Add archive title and desc.

--- resources/views/content/index.php
+++ resources/views/content/index.php
@@ -8,6 +8,8 @@
 ?>
 <main id="main" class="app-main mx-auto max-width-1 px-2 py-4">
 	<?php
+	Hybrid\render_view( 'partials', 'archive-header' );
+
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
