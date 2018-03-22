2b33ab0f7b19a15588997edc160e041dfd1322f2
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-22 14:50:08 +0200

    Add 404.php template.

---
+++ resources/views/content/404.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Content template.
+ *
+ * @package Uuups
+ */
+
+?>
+<main id="main" class="app-main mx-auto max-width-1 px-2 py-4">
+	<section class="entry entry--error mx-auto max-width-2">
+		<header class="entry__header">
+			<h1 class="entry__title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'uuups' ); ?></h1>
+		</header><!-- .page-header -->
+
+		<div class="entry__content">
+			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try search?', 'uuups' ); ?></p>
+			<?php get_search_form(); ?>
+		</div>
+	</section>
+</main>
