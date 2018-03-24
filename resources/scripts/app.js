afbd4f46002dc50935f4f93c888fa7a7703c28f1
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-24 22:08:38 +0200

    Handle focus when menu is open and close menu when pressing Esc.

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -5,9 +5,11 @@
  * navigation support for dropdown menus.
  */
 ( function() {
-	var container, button, menu, links, i, len;
+	var container, button, menu, links, i, len, focusableElements, firstFocusableElement, lastFocusableElement;
 
 	container = document.getElementById( 'js-menu--primary' );
+
+	// Bail if there is no menu.
 	if ( ! container ) {
 		return;
 	}
@@ -31,12 +33,19 @@
 	}
 
 	button.addEventListener( 'click', function() {
-		container.classList.toggle( 'is-toggled' );
-		menu.classList.toggle( 'is-toggled' );
-		button.classList.toggle( 'is-toggled' );
+		toggleMenu();
 
-		let expanded = ( 'false' === button.getAttribute( 'aria-expanded' ) ) ? 'true' : 'false';
-		button.setAttribute( 'aria-expanded', expanded );
+		setFocus();
+	} );
+
+	// Close menu using Esc key.
+	document.addEventListener( 'keyup', function( event ) {
+		if ( 27 === event.keyCode ) {
+			if ( button.classList.contains( 'is-toggled' ) ) {
+				toggleMenu(); // Close menu.
+				button.focus();
+			}
+		}
 	} );
 
 	// Get all the link elements within the menu.
@@ -46,6 +55,58 @@
 	for ( i = 0, len = links.length; i < len; i++ ) {
 		links[i].addEventListener( 'focus', toggleFocus, true );
 		links[i].addEventListener( 'blur', toggleFocus, true );
+	}
+
+	/**
+	 * Toggle menu classes and ARIA.
+	 */
+	function toggleMenu() {
+		container.classList.toggle( 'is-toggled' );
+		menu.classList.toggle( 'is-toggled' );
+		button.classList.toggle( 'is-toggled' );
+
+		let expanded = ( 'false' === button.getAttribute( 'aria-expanded' ) ) ? 'true' : 'false';
+		button.setAttribute( 'aria-expanded', expanded );
+	}
+
+	/**
+	 * Set focus when nav is open.
+	 */
+	function setFocus() {
+
+		// Bail if menu is not open.
+		if ( ! button.classList.contains( 'is-toggled' ) ) {
+			return;
+		}
+
+		// Set focusable elements inside main navigation.
+		focusableElements     = container.querySelectorAll( [ 'a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex^="-"])' ] );
+		firstFocusableElement = focusableElements[0];
+		lastFocusableElement  = focusableElements[focusableElements.length - 1];
+
+		// Redirect last Tab to first focusable element.
+		lastFocusableElement.addEventListener( 'keydown', function( e ) {
+			if ( ( 9 === e.keyCode && ! e.shiftKey ) ) {
+				e.preventDefault();
+				button.focus(); // Set focus on first element - that's actually close menu button.
+			}
+		} );
+
+		// Redirect first Shift+Tab to toggle button element.
+		firstFocusableElement.addEventListener( 'keydown', function( e ) {
+			if ( ( 9 === e.keyCode && e.shiftKey ) ) {
+				e.preventDefault();
+				button.focus(); // Set focus on first element.
+			}
+		} );
+
+		// Redirect Shift+Tab from the toggle button to last focusable element.
+		button.addEventListener( 'keydown', function( e ) {
+			if ( ( 9 === e.keyCode && e.shiftKey ) ) {
+				e.preventDefault();
+				lastFocusableElement.focus(); // Set focus on last element.
+			}
+		} );
 	}
 
 	/**
