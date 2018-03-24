63599f120ce0aae10b3d48aba5645fda52abc2ac
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-24 21:16:10 +0200

    Update theme for the latest HC update. Some of the functions are now in there.

--- app/functions-template.php
+++ app/functions-template.php
@@ -14,57 +14,6 @@
 namespace Uuups;
 
 /**
- * Wrapper for the core WP `locate_template()` function. Runs the templates
- * through `filter_templates()` to change the file paths.
- *
- * @since  1.0.0
- * @access public
- * @param  array|string $templates Template names.
- * @param  bool         $load If true the template file will be loaded if it is found.
- * @param  bool         $require_once Whether to require_once or require.
- * @return string
- */
-function locate_template( $templates, $load = false, $require_once = true ) {
-
-	return \locate_template( filter_templates( (array) $templates ), $load, $require_once );
-}
-
-/**
- * Filters an array of templates and prefixes them with the
- * `/resources/views/` file path.
- *
- * @since  1.0.0
- * @access public
- * @param  array $templates Template filters.
- * @return array
- */
-function filter_templates( $templates ) {
-
-	array_walk( $templates, function( &$template, $key ) {
-
-		$path = 'resources/views';
-
-		$template = ltrim( str_replace( $path, '', $template ), '/' );
-
-		$template = "{$path}/{$template}";
-	} );
-
-	return $templates;
-}
-
-/**
- * Returns the template hierarchy from the theme wrapper.
- *
- * @since  1.0.0
- * @access public
- * @return array
- */
-function get_template_hierarchy() {
-
-	return app()->get( 'wrapper' )->hierarchy;
-}
-
-/**
  * Returns a configuration object.
  *
  * @since  1.0.0
@@ -73,7 +22,6 @@ function get_template_hierarchy() {
  * @return object
  */
 function config( $name ) {
-
 	return app()->get( "config.{$name}" );
 }
 
--- resources/views/index.php
+++ resources/views/index.php
@@ -6,13 +6,13 @@
  */
 
 // Load header/* template.
-Hybrid\render_view( 'header', Uuups\get_template_hierarchy() );
+Hybrid\render_view( 'header', Hybrid\get_template_hierarchy() );
 
 // Load content template.
-Hybrid\render_view( 'content', Uuups\get_template_hierarchy() );
+Hybrid\render_view( 'content', Hybrid\get_template_hierarchy() );
 
 // Load sidebar/* template.
 Hybrid\render_view( 'sidebar', 'primary', [ 'name' => 'primary' ] );
 
 // Load footer/* template.
-Hybrid\render_view( 'footer', Uuups\get_template_hierarchy() );
+Hybrid\render_view( 'footer', Hybrid\get_template_hierarchy() );
