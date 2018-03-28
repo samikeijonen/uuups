31fe86c4ff8b7530333c952dfce517048fd6c5f0
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-28 11:08:52 +0300

    Use singleton method.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -37,7 +37,7 @@ function app() {
 }
 
 // Add configuration.
-app()->add( 'config.theme', function() {
+app()->singleton( 'config.theme', function() {
 	return new Collection( require_once get_parent_theme_file_path( 'config/theme.php' ) );
 } );
 
