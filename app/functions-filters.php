75e855b1c8d775a25e661dcf97af87f52a59721f
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-04-10 08:34:22 +0300

    Add excerpt more filter.

--- app/functions-filters.php
+++ app/functions-filters.php
@@ -42,3 +42,26 @@ add_filter( 'nav_menu_link_attributes', function( $atts, $item, $args, $depth ) 
 
 	return $atts;
 }, 10, 4 );
+
+/**
+ * Replaces "[...]" (appended to automatically generated excerpts) with ... and
+ * a 'Continue reading' link.
+ *
+ * @param string $link Link to single post/page.
+ * @return string 'Continue reading' link prepended with an ellipsis.
+ */
+function excerpt_more( $link ) {
+	if ( is_admin() ) {
+		return $link;
+	}
+
+	$link = sprintf(
+		'<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
+		esc_url( get_permalink( get_the_ID() ) ),
+		/* translators: %s: Name of current post */
+		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'uuups' ), get_the_title( get_the_ID() ) )
+	);
+
+	return ' &hellip; ' . $link;
+}
+add_filter( 'excerpt_more', __NAMESPACE__ . '\excerpt_more' );
