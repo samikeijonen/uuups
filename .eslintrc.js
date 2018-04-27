9d41a373e1bb5c15cc8f79655691c264c6f78f56
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-04-27 16:19:55 +0300

    Take wordpress lints off from eslint rules. They are checked already.

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,44 +49,6 @@ module.exports = {
 	 */
 	rules: {
 		/**
-		 * Enforce spacing inside array brackets.
-		 *
-		 * @standard WDS
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'array-bracket-spacing': ['error', 'always'],
-
-		/**
-		 * Enforce one true brace style.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#blocks-and-curly-braces
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'brace-style': 'error',
-
-		/**
-		 * Require camel case names.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#naming-conventions
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		camelcase: [
-			'error',
-			{
-				properties: 'always',
-			},
-		],
-
-		/**
 		 * Disallow or enforce trailing commas.
 		 *
 		 * @standard WP
@@ -96,27 +58,6 @@ module.exports = {
 		 * @since 1.0
 		 */
 		'comma-dangle': ['error', 'never'],
-
-		/**
-		 * Enforce spacing before and after comma.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#objects
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'comma-spacing': 'error',
-
-		/**
-		 * Enforce one true comma style.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'comma-style': ['error', 'last'],
 
 		/**
 		 * Encourages use of dot notation whenever possible.
@@ -136,174 +77,6 @@ module.exports = {
 		],
 
 		/**
-		 * Enforce newline at the end of file, with no multiple empty lines.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'eol-last': 'error',
-
-		/**
-		 * Require or disallow spacing between function identifiers and their invocations.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'func-call-spacing': 'off',
-
-		/**
-		 * Enforces spacing between keys and values in object literal properties.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'key-spacing': [
-			'error',
-			{
-				beforeColon: false,
-				afterColon: true,
-			},
-		],
-
-		/**
-		 * Enforce spacing before and after keywords.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'keyword-spacing': 'error',
-
-		/**
-		 * Disallow mixed "LF" and "CRLF" as linebreaks.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'linebreak-style': ['error', 'unix'],
-
-		/**
-		 * Enforces empty lines around comments.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'lines-around-comment': [
-			'error',
-			{
-				beforeLineComment: true,
-			},
-		],
-
-		/**
-		 * Disallow mixed spaces and tabs for indentation.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'no-mixed-spaces-and-tabs': 'error',
-
-		/**
-		 * Disallow use of multiline strings.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'no-multi-str': 'error',
-
-		/**
-		 * Disallow multiple empty lines.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'no-multiple-empty-lines': 'error',
-
-		/**
-		 * Disallow use of the with statement.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'no-with': 'error',
-
-		/**
-		 * Require or disallow an newline around variable declarations.
-		 *
-		 * @standard WDS
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'one-var-declaration-per-line': ['error', 'initializations'],
-
-		/**
-		 * Enforce operators to be placed before or after line breaks.
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'operator-linebreak': ['error', 'after'],
-
-		/**
-		 * Require or disallow use of semicolons instead of ASI.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#semicolons
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		semi: ['error', 'always'],
-
-		/**
-		 * Require or disallow space before blocks.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'space-before-blocks': ['error', 'always'],
-
-		/**
-		 * Require or disallow space before function opening parenthesis.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.0
-		 */
-		'space-before-function-paren': ['error', 'never'],
-
-		/**
 		 * Require or disallow space before blocks.
 		 *
 		 * @standard WP
@@ -313,32 +86,6 @@ module.exports = {
 		 * @since  1.1
 		 */
 		'space-in-parens': ['error', 'always'],
-
-		/**
-		 * Require spaces around operators.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#spacing
-		 *
-		 * @author Aubrey Portwood
-		 * @since 1.1
-		 */
-		'space-infix-ops': 'error',
-
-		/**
-		 * Require or disallow spaces before/after unary operators (words on by default, nonwords),
-		 *
-		 * @standard WP
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 */
-		'space-unary-ops': [
-			'error',
-			{
-				overrides: { '!': true },
-			},
-		],
 
 		/**
 		 * Don't force vars to be on top.
@@ -353,17 +100,6 @@ module.exports = {
 		 * @author Aubrey Portwood
 		 */
 		'vars-on-top': 'off',
-
-		/**
-		 * Require or disallow Yoda conditions.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#yoda-conditions
-		 *
-		 * @since  1.0
-		 * @author Aubrey Portwood
-		 */
-		yoda: ['error', 'always'],
 
 		/**
 		 * Always show an error when a variable is created that is never used.
@@ -462,28 +198,6 @@ module.exports = {
 		eqeqeq: 'error',
 
 		/**
-		 * Require that braces be used.
-		 *
-		 * @standard WP
-		 *
-		 * E.g.
-		 *
-		 *     if ( foo ) return;
-		 *
-		 * would be bad, but
-		 *
-		 *     if ( foo ) {
-		 *         return;
-		 *     }
-		 *
-		 * would pass.
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 */
-		curly: 'error',
-
-		/**
 		 * Disallow null comparisons without type-checking operators.
 		 *
 		 * @standard WP
@@ -536,16 +250,5 @@ module.exports = {
 		 * @since 1.1
 		 */
 		'no-undef': 'error',
-
-		/**
-		 * camelCaseAllTheThings.
-		 *
-		 * @standard WP
-		 * @see      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#naming-conventions
-		 *
-		 * @author Aubrey Portwood
-		 * @since  1.1
-		 */
-		camelcase: 'error',
 	},
 };
