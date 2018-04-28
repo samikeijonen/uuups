418dabeabbbfe2b1197f2ddf077e94bca3ad5ab1
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-04-28 15:48:07 +0300

    Add translator comment to footer info.

--- resources/views/footer/default.php
+++ resources/views/footer/default.php
@@ -14,7 +14,10 @@
 		<p class="app-footer__credit text-center color-grey-60 mb-0">
 			<?php esc_html_e( 'Powered by crazy ideas and passion', 'uuups' ); ?>
 			<span class="sep"> &middot; </span>
-			<?php printf( esc_html__( 'Theme %1$s by %2$s', 'uuups' ), 'Uuups', '<a href="https://foxland.fi/">Foxland</a>' ); ?>
+			<?php
+				/* translators: %1$s is theme name, and %2$s is link to theme site. */
+				printf( esc_html__( 'Theme %1$s by %2$s', 'uuups' ), 'Uuups', '<a href="https://foxland.fi/">Foxland</a>' );
+			?>
 		</p>
 	</footer>
 
