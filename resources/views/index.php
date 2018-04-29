b9d7d57d482cf92e0b00c81f7d7c9ac0add66a64
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-04-29 10:37:29 +0300

    Use new get_global_hierarchy function.

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,13 +6,13 @@
  */
 
 // Load header/* template.
-Hybrid\render_view( 'header', Hybrid\get_template_hierarchy() );
+Hybrid\render_view( 'header', Hybrid\get_global_hierarchy() );
 
 // Load content template.
-Hybrid\render_view( 'content', Hybrid\get_template_hierarchy() );
+Hybrid\render_view( 'content', Hybrid\get_global_hierarchy() );
 
 // Load sidebar/* template.
 Hybrid\render_view( 'sidebar', 'primary', [ 'name' => 'primary' ] );
 
 // Load footer/* template.
-Hybrid\render_view( 'footer', Hybrid\get_template_hierarchy() );
+Hybrid\render_view( 'footer', Hybrid\get_global_hierarchy() );
