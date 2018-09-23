58d1fb057962fd2b960cbf18f6ff9d12cdb3fe68
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-09-23 19:14:15 +0300

    Remove add_editor_support since we are rolling our own system.

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -107,7 +107,6 @@ add_action( 'after_setup_theme', function() {
 
 	// Add support for editor styles.
 	add_theme_support( 'editor-styles' );
-	add_editor_style( 'dist/css/style-editor.css' );
 }, 5 );
 
 /**
