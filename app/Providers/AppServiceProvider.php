2f33b63d8510fcadd501bd05c20268408e5578d4
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-11-16 23:14:19 +0200

    Fix comment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,7 +34,7 @@ class AppServiceProvider extends ServiceProvider {
 		// Bind a single instance of our customizer class.
 		$this->app->singleton( Customize::class );
 
-		// Bind the Laravel Mix manifest for cache-busting.
+		// Bind the manifest for cache-busting.
 		$this->app->singleton(
 			'uuups/manifest',
 			function() {
