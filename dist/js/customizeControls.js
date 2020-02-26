e3c6e4be719a8b2ebbb190a7205762234f8b7504
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-02-26 20:29:01 +0200

    Compile assets.

--- dist/js/customizeControls.js
+++ dist/js/customizeControls.js
@@ -93,7 +93,24 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/samikeijonen/local/foxlandhub/app/public/wp-content/themes/uuups/assets/js/customize-controls.js'");
+/**
+ * Customize controls script.
+ *
+ * This file handles the JavaScript for the controls panel in the customizer.
+ * Any includes or imports should be handled in this file. The final result gets
+ * saved back into `dist/js/customize-controls.js`.
+ *
+ * @package Uuups
+ */
+// Hybrid Customize controls.
+//
+// Uncomment any of the below to import scripts for specific controls if using
+// the `hybrid-customize` add-on.
+// import checkboxMultiple from 'hybrid-customize/js/controls/checkbox-multiple.js';
+// import palette          from 'hybrid-customize/js/controls/palette.js';
+// import radioImage       from 'hybrid-customize/js/controls/radio-image.js';
+// import selectGroup      from 'hybrid-customize/js/controls/select-group.js';
+// import selectMultiple   from 'hybrid-customize/js/controls/select-multiple.js';
 
 /***/ })
 
